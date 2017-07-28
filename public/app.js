b925d8b45d34987d036d3dc75604a89dcd28d11f
pbywater <philippabywater@gmail.com>
2017-07-28 13:09:36 +0100

    changing config.env reference in the gitignore
    relates to #22

--- public/app.js
+++ public/app.js
@@ -19,7 +19,9 @@ function updateDom (err, data) {
     var users = JSON.parse(data);
     var table = document.getElementById("users-table");
     /* create a row in table for each user returned from DB */
+    console.log('hello', users);
     users.forEach(function(user) {
+      console.log('user is ', user);
       var row = document.createElement("tr");
       var name = document.createElement("td");
       name.innerHTML = user.name;
