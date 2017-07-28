bbdd16d6be5d911b1b2ae4f95a45351a4b32e6bf
pbywater <philippabywater@gmail.com>
2017-07-28 17:13:20 +0100

    moving database folder

--- src/handlers/getData.js
+++ src/handlers/getData.js
@@ -1,4 +1,4 @@
-const dbConnection = require('../../database/db_connection.js');
+const dbConnection = require('../database/db_connection.js');
 
 const getData = (cb) =>{
     dbConnection.query(`SELECT * FROM users`
--- src/handlers/postData.js
+++ src/handlers/postData.js
@@ -1,7 +1,7 @@
-const db_connection = require('../../database/db_connection');
+const dbConnection = require('../database/db_connection.js');
 
 const postData = (name, location, cb) => {
-  db_connection.query('INSERT INTO users (name, location) VALUES ($1, $2)', [name, location], (err, res) => {
+  dbConnection.query('INSERT INTO users (name, location) VALUES ($1, $2)', [name, location], (err, res) => {
     if (err) {
       return cb(err);
     }
