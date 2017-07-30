bf3593ca03f9dbb749b1f6cffe4e2827a7872a15
pbywater <philippabywater@gmail.com>
2017-07-30 20:46:08 +0100

    adding getData
    relates to #26

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,1 +1,16 @@
 // Add code below to query your database
+const dbConnection = require('../database/db_connection.js');
+
+const getData = (cb) =>{
+    dbConnection.query(`SELECT * FROM users`
+    , (err, res) => {
+    if {
+      (err) cb(err);
+    }
+    else {
+      cb(null, res.rows);
+    }
+  });
+};
+
+module.exports = getData;
