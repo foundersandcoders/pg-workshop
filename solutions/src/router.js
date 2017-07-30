92074ddaea4705869a3f59d2f0781892495f8e0d
pbywater <philippabywater@gmail.com>
2017-07-30 20:39:18 +0100

    adding solution to folder in master
    relates to #26

---
+++ solutions/src/router.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const users = require('./static.js');
+const fs = require('fs');
+const pg = require('pg');
+
+const router = (request, response) => {
+  const endpoint = request.url.split('/')[1];
+
+  if (endpoint === '') {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+      if(error) {
+        console.log(error);
+        return;
+      } else {
+        response.end(file);
+      }
+    });
+  } else if (endpoint === "users") {
+    // Replace the 3 lines below below with your own function that gets data from your database
+    const output = JSON.stringify(users);
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(output);
+  } else {
+    const fileName = request.url;
+    const fileType = request.url.split(".")[1];
+    response.writeHead(200, {"Content-Type": "text/" + fileType});
+    fs.readFile(__dirname + "/../public" + fileName, function(error, file) {
+      if(error) {
+        console.log(error);
+        return;
+      } else {
+        response.end(file);
+      }
+    });
+  }
+};
+
+module.exports = router;
