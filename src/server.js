b054ace2821472e58a152bdd7a0c2bf598f4df23
pbywater <philippabywater@gmail.com>
2017-07-31 20:53:00 +0100

    fixing indentation
    relates to #29

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const router = require('./router.js');
 const server = http.createServer(router);
 const port = process.env.PORT || 5000;
 
-    server.listen(port, () => {
-        console.log(`Magic happens on port ${port}`);
-    });
+server.listen(port, () => {
+    console.log(`Magic happens on port ${port}`);
+});
