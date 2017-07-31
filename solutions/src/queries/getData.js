3532e309538c8bbfff9ee6785672f92f76023f27
pbywater <philippabywater@gmail.com>
2017-07-31 20:51:57 +0100

    fixing syntax error
    relates to #29

--- solutions/src/queries/getData.js
+++ solutions/src/queries/getData.js
@@ -3,8 +3,8 @@ const dbConnection = require('../database/db_connection.js');
 
 const getData = (cb) => {
     dbConnection.query(`SELECT * FROM users`, (err, res) => {
-        if {
-            (err) cb(err);
+        if (err) {
+            cb(err);
         } else {
             cb(null, res.rows);
         }
