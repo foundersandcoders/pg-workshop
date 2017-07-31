4cfab982f5d82c311c2e797854566f5a565ccef6
pbywater <philippabywater@gmail.com>
2017-07-31 18:49:57 +0100

    removing redundant function call in server.js
    relates to #29 #30

--- solutions/src/server.js
+++ solutions/src/server.js
@@ -4,10 +4,6 @@ const router = require('./router.js');
 const server = http.createServer(router);
 const port = process.env.PORT || 5000;
 
-const startServer = () => {
     server.listen(port, () => {
         console.log(`Magic happens on port ${port}`);
     });
-}
-
-startServer();
