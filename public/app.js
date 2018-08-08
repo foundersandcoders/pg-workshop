a415da50a88b6c4078c7efaad135a36c5f1c6024
i2xzy <yitzchokr@live.com>
2018-08-08 12:50:51 +0100

    add public to static file endpoints, add css to main workshop

--- public/app.js
+++ public/app.js
@@ -1,35 +1,35 @@
 /* generic XHR request */
 function request(url, cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            cb(null, xhr.responseText);
-        } else {
-            cb("error" + xhr.responseType);
-        }
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      cb(null, xhr.responseText);
+    } else {
+      cb('error' + xhr.responseType);
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
 }
 
 function updateDom(err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        var users = JSON.parse(data);
-        var table = document.getElementById("users-table");
-        /* create a row in table for each user returned from DB */
-        users.forEach(function(user) {
-            var row = document.createElement("tr");
-            var name = document.createElement("td");
-            name.innerHTML = user.name;
-            row.appendChild(name);
-            var location = document.createElement("td");
-            location.innerHTML = user.location;
-            row.appendChild(location);
-            table.appendChild(row);
-        });
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    var users = JSON.parse(data);
+    var table = document.getElementById('users-table');
+    /* create a row in table for each user returned from DB */
+    users.forEach(function(user) {
+      var row = document.createElement('tr');
+      var name = document.createElement('td');
+      name.innerHTML = user.name;
+      row.appendChild(name);
+      var location = document.createElement('td');
+      location.innerHTML = user.location;
+      row.appendChild(location);
+      table.appendChild(row);
+    });
+  }
 }
 
 request('/users', updateDom);
