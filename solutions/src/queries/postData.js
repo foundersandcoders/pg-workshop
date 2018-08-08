6709978aed5ca87775a18a7df0af6f44eb4b025e
i2xzy <yitzchokr@live.com>
2018-08-08 13:11:26 +0100

    minor refactor for database files in solution

--- solutions/src/queries/postData.js
+++ solutions/src/queries/postData.js
@@ -1,13 +1,17 @@
 const dbConnection = require('../database/db_connection.js');
 
 const postData = (name, location, cb) => {
-    dbConnection.query('INSERT INTO users (name, location) VALUES ($1, $2)', [name, location], (err, res) => {
-        if (err) {
-            return cb(err);
-        } else {
-            cb(null, res);
-        }
-    })
-}
+  dbConnection.query(
+    'INSERT INTO users (name, location) VALUES ($1, $2)',
+    [name, location],
+    (err, res) => {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, res);
+      }
+    }
+  );
+};
 
 module.exports = postData;
