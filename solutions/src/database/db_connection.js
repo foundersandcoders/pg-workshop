07e0e13c3f588224db4d7d50666b61f3cfba2c61
Kanna'n Hassouna <knanahassouna2@gmail.com>
2020-01-15 13:56:57 +0200

    use the new url class and remove deprecated code

--- solutions/src/database/db_connection.js
+++ solutions/src/database/db_connection.js
@@ -1,21 +1,20 @@
-const { Pool } = require('pg');
-const url = require('url');
-require('env2')('./config.env');
+const { Pool } = require("pg");
+const { URL } = require("url");
+require("env2")("./config.env");
 
 if (!process.env.DB_URL)
-  throw new Error('Enviroment variable DB_URL must be set');
+  throw new Error("Enviroment variable DB_URL must be set");
 
-const params = url.parse(process.env.DB_URL);
-const [username, password] = params.auth.split(':');
+const url = new URL(process.env.DB_URL);
 
 const options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
+  host: url.hostname,
+  port: url.port,
+  database: url.pathname.split("/")[1],
   max: process.env.DB_MAX_CONNECTIONS || 2,
-  user: username,
-  password,
-  ssl: params.hostname !== 'localhost'
+  user: url.username,
+  password: url.password,
+  ssl: url.hostname !== "localhost"
 };
 
 module.exports = new Pool(options);
